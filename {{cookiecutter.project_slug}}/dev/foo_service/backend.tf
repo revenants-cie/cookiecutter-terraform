49a7cbb951d5984fcc75252c39960f04b75e207d
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2020-05-05 20:31:24 -0700

    Update versions, formatting

--- {{cookiecutter.project_slug}}/dev/foo_service/backend.tf
+++ {{cookiecutter.project_slug}}/dev/foo_service/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     region = "us-east-1"
     key    = "{{ cookiecutter.project_slug }}/dev/foo_service/terraform.tfstate"
-      bucket = "{{ cookiecutter.states_bucket }}"
+    bucket = "{{ cookiecutter.states_bucket }}"
   }
 }
--- {{cookiecutter.project_slug}}/staging/foo_service/backend.tf
+++ {{cookiecutter.project_slug}}/staging/foo_service/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region = "us-east-1"
-      key    = "{{ cookiecutter.project_slug }}/staging/foo_service/terraform.tfstate"
+    key    = "{{ cookiecutter.project_slug }}/staging/foo_service/terraform.tfstate"
     bucket = "{{ cookiecutter.states_bucket }}"
   }
 }
